8ad6345fbe81abc4187b685834937fdd0624b17c
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-25 18:53:26 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+let x="Helo";
+let y="World<br>" ;
+
+
+/* Methode 1 */
+console.log(x+y)
+let z =(x+y);
+document.write(z);
+/* Methode 2 */
+console.log(x+" "+y)
+let n=(x+" "+y);
+document.write(n);
+
+/* Methode 3 */
+x+="World";
+
+document.write(x);
